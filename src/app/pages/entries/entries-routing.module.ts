f2b581e0b8f113f8059c5ecfe71f03ccd61ba3b0
LeonardoFFerreira <devleofreitas@gmail.com>
2021-12-08 20:52:09 -0300

    improving EntryFormComponent

--- src/app/pages/entries/entries-routing.module.ts
+++ src/app/pages/entries/entries-routing.module.ts
@@ -1,7 +1,8 @@
-import { EntryListComponent } from './entry-list/entry-list.component';
-import { EntryFormComponent } from './entry-form/entry-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { EntryListComponent } from './entry-list/entry-list.component';
+import { EntryFormComponent } from './entry-form/entry-form.component';
 
 const routes: Routes = [
   { path: '', component: EntryListComponent },
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,15 +1,24 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
+
+import { EntriesRoutingModule } from './entries-routing.module';
+
+import { CalendarModule } from "primeng/calendar";
+
+import { IMaskModule } from "angular-imask";
 
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule
   ]
 })
 export class EntriesModule { }
